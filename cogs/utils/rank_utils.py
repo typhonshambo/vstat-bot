459f369f6d06c30af74cedead082b05e89f75b79
Shambo Chowdhury <54593764+typhonshambo@users.noreply.github.com>
2022-01-28 21:00:10 +0530

    removed normal commands
    - as there is no point of Normal commands on discord so removing it :) will work on slash commands

--- cogs/utils/rank_utils.py
+++
@@ -1,9 +0,0 @@
-import  requests
-
-
-def getrank(region, user_id):
-    req_data = requests.get(f"https://api.henrikdev.xyz/valorant/v2/by-puuid/mmr/{region}/{user_id}") 
-    whole_data = req_data.json()
-
-    
-    return whole_data
